567397ed1594a8281fac4ca03907dcce856eb5b3
Zephyr <45801099+zephyr-c@users.noreply.github.com>
2024-06-18 18:29:04 -0700

    Add endpoint tests

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify
 from ReceiptProcessor import ReceiptProcessor
 import uuid
-import json
+
+from models.receipt import ReceiptSchema
 
 app = Flask(__name__)
 
 receipts = {
-    '13b989e1-66f5-41cd-b71f-dffe26049cf4': 35
+    '13b989e1-66f5-41cd-b71f-dffe26049cf4': {'points': 35}
 }
 
 
@@ -14,20 +15,25 @@ receipts = {
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
 @app.route("/receipts/process", methods=['GET', 'POST'])
 def process_receipt():
+    schema = ReceiptSchema()
     new_receipt_data = request.get_json()
-    new_receipt = ReceiptProcessor(new_receipt_data)
+    processed_receipt = ReceiptProcessor(new_receipt_data)
+    new_receipt = schema.dump(processed_receipt)
     receipt_id = str(uuid.uuid4())
     receipts[receipt_id] = new_receipt
-    return receipt_id
+
+    return jsonify(id=receipt_id)
+
 
 @app.route("/receipts/<id>/points", methods=['GET'])
 def get_receipt_points(id):
-    print(receipts)
     receipt = receipts[id]
-    return jsonify(points=receipt.points)
+    return jsonify(points=receipt['points'])
     # return jsonify(points=receipts[id])
+
 
 @app.route("/receipts/all", methods=['GET'])
 def get_all_receipts():
--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,14 +1,22 @@
 import json
 
+
 def test_hello_world(test_client):
     response = test_client.get('/')
     assert response.status_code == 200
     assert "Hello, World!" in response.text
 
-def test_process_receipt(test_client):
+
+def test_process_new_receipt(test_client):
     with open('examples/simple-receipt.json') as jsonfile:
         data = jsonfile.read()
     receipt_data = json.loads(data)
     response = test_client.post('/receipts/process', json=receipt_data)
     assert response.status_code == 200
-    assert len(response.text) == 36
+    assert 'id' in response.json
+    assert len(response.json['id']) == 36
+
+def test_get_existing_points(test_client):
+    receipt_id = '13b989e1-66f5-41cd-b71f-dffe26049cf4'
+    response = test_client.get(f'receipts/{receipt_id}/points')
+    assert response.json['points'] == 35
